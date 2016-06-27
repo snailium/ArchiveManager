5aee8572a9d6a4f7451e7af3b43739b21197ead7
Guannan Wang <Guannan Wang>
2016-06-27 02:44:02 -0400

    Added coalesced ini editor for Mirror's Edge. Need to be merged to main program.

---
+++ CoalescedCooker/CoalescedCooker/MainFrame.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CoalescedCooker
+{
+    partial class CoalescedCooker
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CoalescedCooker));
+            this.UncookButton = new System.Windows.Forms.Button();
+            this.CookButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // UncookButton
+            // 
+            this.UncookButton.Location = new System.Drawing.Point(21, 18);
+            this.UncookButton.Name = "UncookButton";
+            this.UncookButton.Size = new System.Drawing.Size(117, 46);
+            this.UncookButton.TabIndex = 0;
+            this.UncookButton.Text = "&Uncook";
+            this.UncookButton.UseVisualStyleBackColor = true;
+            this.UncookButton.Click += new System.EventHandler(this.UncookButton_Click);
+            // 
+            // CookButton
+            // 
+            this.CookButton.Location = new System.Drawing.Point(144, 18);
+            this.CookButton.Name = "CookButton";
+            this.CookButton.Size = new System.Drawing.Size(117, 46);
+            this.CookButton.TabIndex = 1;
+            this.CookButton.Text = "&Cook";
+            this.CookButton.UseVisualStyleBackColor = true;
+            this.CookButton.Click += new System.EventHandler(this.CookButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(267, 18);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(117, 46);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "Coalesced.ini";
+            this.openFileDialog.ReadOnlyChecked = true;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileName = "Coalesced.ini";
+            // 
+            // CoalescedCooker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 83);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CookButton);
+            this.Controls.Add(this.UncookButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CoalescedCooker";
+            this.ShowIcon = false;
+            this.Text = "Coalesced Cooker 0.1 by Snailium";
+            this.Load += new System.EventHandler(this.CoalescedCooker_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UncookButton;
+        private System.Windows.Forms.Button CookButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ CoalescedCooker/CoalescedCooker/MainFrame.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CoalescedCooker
+{
+    public partial class CoalescedCooker : Form
+    {
+        public CoalescedCooker()
+        {
+            InitializeComponent();
+        }
+
+        private void CoalescedCooker_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int ReadIntFromFile(FileStream TheFile, long offset_from_beginning)
+        {
+            byte[] tmpint = ReadBytesFromFile(TheFile, offset_from_beginning, 4);
+            if (BitConverter.IsLittleEndian) Array.Reverse(tmpint);
+            return BitConverter.ToInt32(tmpint, 0);
+        }
+
+        private void WriteIntToFile(FileStream TheFile, int value)
+        {
+            byte[] temp = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian) Array.Reverse(temp);
+            WriteBytesToFile(TheFile, temp, 4);
+        }
+
+        private float ReadFloatFromFile(FileStream TheFile, long offset_from_beginning)
+        {
+            byte[] tmpint = ReadBytesFromFile(TheFile, offset_from_beginning, 4);
+            if (BitConverter.IsLittleEndian) Array.Reverse(tmpint);
+            return BitConverter.ToSingle(tmpint, 0);
+        }
+
+        private void WriteFloatToFile(FileStream TheFile, float value)
+        {
+            byte[] temp = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian) Array.Reverse(temp);
+            WriteBytesToFile(TheFile, temp, 4);
+        }
+
+        private byte[] ReadBytesFromFile(FileStream TheFile, long offset_from_beginning, int num_bytes)
+        {
+            byte[] tmpint = new byte[num_bytes];
+            long ret = TheFile.Seek(offset_from_beginning, SeekOrigin.Begin);
+            TheFile.Read(tmpint, 0, num_bytes);
+            return tmpint;
+        }
+
+        private void WriteBytesToFile(FileStream TheFile, byte[] value, int num_bytes = 0)
+        {
+            byte[] temp = value;
+            int length = num_bytes;
+            if (length == 0) length = temp.Length;
+            //long ret = NewFile.Seek(offset_from_beginning, SeekOrigin.Begin);
+            TheFile.Write(temp, 0, length);
+        }
+
+        private string ReadStringFromFile(FileStream TheFile, long offset_from_beginning, int num_bytes, byte delimiter = 0x00)
+        {
+            byte[] temp = ReadBytesFromFile(TheFile, offset_from_beginning, num_bytes);
+            if (temp[temp.Length - 1] == delimiter) Array.Resize(ref temp, temp.Length - 1);
+            return ASCIIEncoding.ASCII.GetString(temp);
+        }
+
+        private void WriteStringToFile(FileStream TheFile, string value, byte delimiter = 0x00)
+        {
+            byte[] temp = ASCIIEncoding.ASCII.GetBytes(value);
+            WriteBytesToFile(TheFile, temp, temp.Length);
+            if (temp[temp.Length - 1] != delimiter)
+            {
+                byte[] tmp = { delimiter };
+                WriteBytesToFile(TheFile, tmp, tmp.Length);
+            }
+        }
+        
+        private long CopyBlock(FileStream FromFile, FileStream ToFile, long StartOffset, int CopyLength)
+        {
+            byte[] temp = ReadBytesFromFile(FromFile, StartOffset, CopyLength);
+            WriteBytesToFile(ToFile, temp);
+            return CopyLength;
+        }
+
+        private void UncookButton_Click(object sender, EventArgs e)
+        {
+            string FileName, FolderName;
+            FileStream INIFile;
+            long Offset;
+
+            openFileDialog.Title = "Open Coalesced.ini";
+            openFileDialog.Filter = "Coalesced.ini|Coalesced.ini";
+            DialogResult FileResult = openFileDialog.ShowDialog();
+            
+            folderBrowserDialog.Description = "Choose folder for unpacked files";
+            DialogResult FolderResult = folderBrowserDialog.ShowDialog();
+
+            // OK button was pressed.
+            if (FileResult == DialogResult.OK && FolderResult == DialogResult.OK)
+            {
+                FileName = openFileDialog.FileName;
+                FolderName = folderBrowserDialog.SelectedPath;
+                if(FolderName.LastIndexOf('\\') != FolderName.Length - 1)
+                    FolderName = FolderName + "\\";
+            
+                try
+                {
+                    FileStream idxFile = new FileStream(FolderName + "files.idx", FileMode.Create, FileAccess.Write);
+                    INIFile = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+                    if (INIFile != null && INIFile.CanRead &&
+                        idxFile != null && idxFile.CanWrite)
+                    {
+                        // Read number of files
+                        Offset = 0;
+                        int numFiles = ReadIntFromFile(INIFile, Offset);
+                        Offset += 4;
+                        numFiles = numFiles / 2; // Actual value is half
+                        WriteIntToFile(idxFile, numFiles);
+                        
+                        for (int i = 0; i < numFiles; i++)
+                        {
+                            FileStream subFile;
+                            // Read File Name
+                            int lenString = ReadIntFromFile(INIFile, Offset);
+                            Offset += 4;
+                            string subPath = ReadStringFromFile(INIFile, Offset, lenString);
+                            Offset += lenString;
+                            WriteIntToFile(idxFile, lenString);
+                            WriteStringToFile(idxFile, subPath);
+                            string subName = subPath.Substring(subPath.LastIndexOf('\\') + 1);
+                            //MessageBox.Show(String.Format("{0:X}", i) + " SubFileName: " + FolderName + subName, "Debug", MessageBoxButtons.OK);
+                            subFile = new FileStream(FolderName + subName, FileMode.Create, FileAccess.Write);
+                            if (subFile != null && subFile.CanWrite)
+                            {
+                                int lenFile = ReadIntFromFile(INIFile, Offset);
+                                Offset += 4;
+                                CopyBlock(INIFile, subFile, Offset, lenFile-1);
+                                byte[] temp = ReadBytesFromFile(INIFile, Offset, 1); // Disposal the last byte (0x00)
+                                Offset += lenFile;
+                                subFile.Close();
+                            }
+                            else
+                            {
+                                MessageBox.Show(subName + ": File is not writeable!?", "Error", MessageBoxButtons.OK);
+                                INIFile.Close();
+                                idxFile.Close();
+                                return;
+                            }
+                        }
+                        INIFile.Close();
+                        idxFile.Close();
+                        MessageBox.Show("Unpacked files are saved at \n" + FolderName, "Unpack", MessageBoxButtons.OK);
+                    }
+                    else
+                    {
+                        if (INIFile == null || !INIFile.CanRead) MessageBox.Show(FileName + ": File is not readable!?", "Error", MessageBoxButtons.OK);
+                        if (idxFile == null || !idxFile.CanWrite) MessageBox.Show(FolderName + "files.idx" + ": File is not writeable!?", "Error", MessageBoxButtons.OK);
+                        if (idxFile != null) idxFile.Close();
+                        if (INIFile != null) INIFile.Close();
+                    }
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("An error occurred while attempting to load the file. The error is: "
+                                    + System.Environment.NewLine + exp.ToString() + System.Environment.NewLine);
+                }
+            }
+
+            // Cancel button was pressed.
+            else
+            {
+                return;
+            }
+
+        }
+
+        private void CookButton_Click(object sender, EventArgs e)
+        {
+            string FileName, FolderName;
+            FileStream INIFile;
+            long Offset;
+
+            folderBrowserDialog.Description = "Choose folder containing ini files";
+            DialogResult FolderResult = folderBrowserDialog.ShowDialog();
+
+            saveFileDialog.Title = "Save Coalesced.ini";
+            saveFileDialog.Filter = "Coalesced.ini|Coalesced.ini";
+            DialogResult FileResult = saveFileDialog.ShowDialog();
+            
+            // OK button was pressed.
+            if (FileResult == DialogResult.OK && FolderResult == DialogResult.OK)
+            {
+                FileName = saveFileDialog.FileName;
+                FolderName = folderBrowserDialog.SelectedPath;
+                if (FolderName.LastIndexOf('\\') != FolderName.Length - 1)
+                    FolderName = FolderName + "\\";
+
+                try
+                {
+                    FileStream idxFile = new FileStream(FolderName + "files.idx", FileMode.Open, FileAccess.Read);
+                    INIFile = new FileStream(FileName, FileMode.Create, FileAccess.Write);
+                    if (INIFile != null && INIFile.CanWrite &&
+                        idxFile != null && idxFile.CanRead)
+                    {
+                        // Read number of files
+                        Offset = 0;
+                        int numFiles = ReadIntFromFile(idxFile, Offset);
+                        Offset += 4;
+                        WriteIntToFile(INIFile, (numFiles * 2));
+
+                        for (int i = 0; i < numFiles; i++)
+                        {
+                            FileStream subFile;
+                            // Read File Name
+                            int lenString = ReadIntFromFile(idxFile, Offset);
+                            Offset += 4;
+                            string subPath = ReadStringFromFile(idxFile, Offset, lenString);
+                            Offset += lenString;
+                            WriteIntToFile(INIFile, lenString);
+                            WriteStringToFile(INIFile, subPath);
+                            string subName = subPath.Substring(subPath.LastIndexOf('\\') + 1);
+                            //MessageBox.Show(String.Format("{0:X}", i) + " SubFileName: " + FolderName + subName, "Debug", MessageBoxButtons.OK);
+                            subFile = new FileStream(FolderName + subName, FileMode.Open, FileAccess.Read);
+                            if (subFile != null && subFile.CanRead)
+                            {
+                                int lenFile = (int)(subFile.Length + 1);
+                                WriteIntToFile(INIFile, lenFile);
+                                CopyBlock(subFile, INIFile, 0, lenFile - 1);
+                                byte[] delimiter = { 0x00 };
+                                WriteBytesToFile(INIFile, delimiter); // Disposal the last byte (0x00)
+                                subFile.Close();
+                            }
+                            else
+                            {
+                                MessageBox.Show(subName + ": File is not readable!?", "Error", MessageBoxButtons.OK);
+                                INIFile.Close();
+                                idxFile.Close();
+                                return;
+                            }
+                        }
+                        INIFile.Close();
+                        idxFile.Close();
+                        MessageBox.Show("Coalesced.ini is saved at \n" + FileName, "Unpack", MessageBoxButtons.OK);
+                    }
+                    else
+                    {
+                        if (INIFile == null || !INIFile.CanWrite) MessageBox.Show(FileName + ": File is not writeable!?", "Error", MessageBoxButtons.OK);
+                        if (idxFile == null || !idxFile.CanRead) MessageBox.Show(FolderName + "files.idx" + ": File is not readable!?", "Error", MessageBoxButtons.OK);
+                        if (idxFile != null) idxFile.Close();
+                        if (INIFile != null) INIFile.Close();
+                    }
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("An error occurred while attempting to load the file. The error is: "
+                                    + System.Environment.NewLine + exp.ToString() + System.Environment.NewLine);
+                }
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
